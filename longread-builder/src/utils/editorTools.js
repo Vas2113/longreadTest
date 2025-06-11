345888b7283c9b89a04fe51832d07ea0711956a6
Vas2113 <ololol2113@mail.ru>
2025-06-11 14:10:41 +0500

    uploadImage

--- longread-builder/src/utils/editorTools.js
+++ longread-builder/src/utils/editorTools.js
@@ -2,130 +2,11 @@ import Header from '@editorjs/header';
 import List from '@editorjs/list';
 import Image from '@editorjs/image';
 import Quote from '@editorjs/quote';
-// import Embed from '@editorjs/embed';
 import Table from '@editorjs/table';
 import Warning from '@editorjs/warning';
 import Link from '@editorjs/link';
 import Code from '@editorjs/code';
-// import Marker from '@editorjs/marker';
-// import InlineCode from '@editorjs/inline-code';
 import Paragraph from '@editorjs/paragraph';
-
-class CustomImageTool {
-  static get toolbox() {
-    return {
-      title: 'Изображение',
-      icon: '<svg width="17" height="15" viewBox="0 0 17 15" xmlns="http://www.w3.org/2000/svg"><path d="M10.5 0L7 5.5l-3-4L0 11h17L10.5 0z" fill="#000"/><circle cx="13" cy="5" r="2" fill="#000"/></svg>',
-    };
-  }
-
-  constructor({ data, api }) {
-    this.api = api;
-    this.data = data || {};
-    this.wrapper = undefined;
-  }
-
-  render() {
-    this.wrapper = document.createElement('div');
-    this.wrapper.innerHTML = `
-      <div class="cdx-image">
-        ${
-          this.data.url
-            ? `
-          <div class="cdx-image__picture">
-            <img src="${this.data.url}" alt="${this.data.caption || ''}">
-          </div>
-          ${
-            this.data.caption
-              ? `
-            <div class="cdx-image__caption" contenteditable="true">
-              ${this.data.caption}
-            </div>
-          `
-              : ''
-          }
-        `
-            : `
-          <div class="cdx-image__placeholder">
-            <label class="cdx-button">
-              <input type="file" accept="image/*" style="display: none;">
-              Выберите изображение
-            </label>
-          </div>
-        `
-        }
-      </div>
-    `;
-
-    const fileInput = this.wrapper.querySelector('input[type="file"]');
-    if (fileInput) {
-      fileInput.addEventListener('change', (e) => this.handleFileSelect(e));
-    }
-
-    const caption = this.wrapper.querySelector('.cdx-image__caption');
-    if (caption) {
-      caption.addEventListener('input', () => {
-        this.data.caption = caption.innerHTML;
-      });
-    }
-
-    return this.wrapper;
-  }
-
-  handleFileSelect(event) {
-    const file = event.target.files[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      this.data.url = e.target.result;
-      this.wrapper.innerHTML = `
-        <div class="cdx-image">
-          <div class="cdx-image__picture">
-            <img src="${this.data.url}" alt="">
-          </div>
-          <div class="cdx-image__caption" contenteditable="true">
-            Введите подпись
-          </div>
-        </div>
-      `;
-
-      const caption = this.wrapper.querySelector('.cdx-image__caption');
-      caption.addEventListener('input', () => {
-        this.data.caption = caption.innerHTML;
-      });
-    };
-    reader.readAsDataURL(file);
-  }
-
-  save() {
-    return this.data;
-  }
-
-  static get pasteConfig() {
-    return {
-      patterns: {
-        image: /https?:\/\/\S+\.(gif|jpe?g|tiff?|png|webp|bmp)/i,
-      },
-      handler: (item) => {
-        return new Promise((resolve) => {
-          fetch(item.data)
-            .then((response) => response.blob())
-            .then((blob) => {
-              const reader = new FileReader();
-              reader.onload = (e) => {
-                resolve({
-                  url: e.target.result,
-                  caption: '',
-                });
-              };
-              reader.readAsDataURL(blob);
-            });
-        });
-      },
-    };
-  }
-}
 
 export const editorTools = {
   header: {
@@ -145,8 +26,43 @@ export const editorTools = {
     inlineToolbar: true,
   },
   image: {
-    class: CustomImageTool,
-    // class: Image,
+    class: Image,
+    config: {
+      uploader: {
+        uploadByFile(file) {
+          return new Promise((resolve) => {
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+              resolve({
+                success: 1,
+                file: {
+                  url: e.target.result, // base64 строка
+                  // Дополнительные метаданные:
+                  name: file.name,
+                  size: file.size,
+                },
+              });
+            };
+
+            reader.readAsDataURL(file);
+          });
+        },
+
+        uploadByUrl(url) {
+          // Для URL просто возвращаем тот же URL
+          return Promise.resolve({
+            success: 1,
+            file: { url },
+          });
+        },
+      },
+
+      // Дополнительные настройки
+      captionPlaceholder: 'Добавьте подпись...',
+      buttonContent: 'Загрузить изображение',
+      types: 'image/jpeg,image/png,image/gif',
+    },
   },
   quote: {
     class: Quote,
@@ -156,15 +72,7 @@ export const editorTools = {
       captionPlaceholder: 'Цитата автора',
     },
   },
-  // embed: {
-  //   class: Embed,
-  //   config: {
-  //     services: {
-  //       youtube: true,
-  //       coub: true,
-  //     },
-  //   },
-  // },
+
   table: {
     class: Table,
     inlineToolbar: true,
@@ -184,10 +92,4 @@ export const editorTools = {
   code: {
     class: Code,
   },
-  // marker: {
-  //   class: Marker,
-  // },
-  // inlineCode: {
-  //   class: InlineCode,
-  // },
 };
